e17f4eca997542cc86b3a7a7fd3dc24bed1e1e34
MeFerdi <ferdinandodhis254@gmail.com>
2025-01-13 23:36:04 +0300

    feat: delete password and add location data model

--- backend/models/farmer.go
+++ backend/models/farmer.go
@@ -7,11 +7,10 @@ import (
 type Farmer struct {
 	gorm.Model
 
-	ID uint `gorm:"primaryKey"`
-
+	ID          uint   `gorm:"primaryKey"`
 	Name        string `json:"name"`
-	Password    string `json:"password"`
 	PhoneNumber string `json:"phoneNumber"`
+	Location    string `json:"location"`
 }
 
 // CreateFarmer inserts a new farmer into the database
@@ -24,4 +23,16 @@ func GetAllFarmers(db *gorm.DB) ([]Farmer, error) {
 	var farmers []Farmer
 	err := db.Find(&farmers).Error
 	return farmers, err
+}
+
+// GetFarmerByID retrieves a farmer by their ID
+func GetFarmerByID(db *gorm.DB, id uint) (*Farmer, error) {
+	var farmer Farmer
+	err := db.First(&farmer, id).Error
+	return &farmer, err
+}
+
+// UpdateFarmer updates the farmer's profile in the database
+func (f *Farmer) UpdateFarmer(db *gorm.DB) error {
+	return db.Save(f).Error
 }
