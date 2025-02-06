9400fe7ec8d50bfee8bc8135bc2b2f76477442c8
MeFerdi <ferdinandodhis254@gmail.com>
2025-02-06 16:04:42 +0300

    feat(fix): add update profile handler

--- backend/controllers/user_controller.go
+++ backend/controllers/user_controller.go
@@ -24,3 +24,37 @@ func DeleteUser(c *gin.Context) {
 
 	c.JSON(http.StatusNoContent, nil)
 }
+
+func UpdateUserProfile(c *gin.Context) {
+    var user models.User
+    if err := db.First(&user, c.Param("id")).Error; err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"message": "User not found"})
+        return
+    }
+
+    var input struct {
+        FirstName   string `json:"first_name"`
+        LastName    string `json:"last_name"`
+        Email       string `json:"email"`
+        PhoneNumber string `json:"phone_number"`
+        Location    string `json:"location"`
+    }
+
+    if err := c.ShouldBindJSON(&input); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid input"})
+        return
+    }
+
+    user.FirstName = input.FirstName
+    user.LastName = input.LastName
+    user.Email = input.Email
+    user.PhoneNumber = input.PhoneNumber
+    user.Location = input.Location
+
+    if err := db.Save(&user).Error; err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to update user"})
+        return
+    }
+
+    c.JSON(http.StatusOK, user)
+}
