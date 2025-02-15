e3b4b7e648a1a49e7fce9c3ca05e1b40ce3d9d35
MeFerdi <ferdinandodhis254@gmail.com>
2025-02-15 22:46:11 +0300

    feat(fix): remove CRUD handler endpoint for farmer and retailer

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -7,9 +7,6 @@ import (
 )
 
 func SetupRoutes(r *gin.Engine) {
-	// Farmer Routes
-	r.GET("/api/farmers", controllers.GetFarmers)
-	r.POST("/api/farmers", controllers.CreateFarmer)
 
 	// Product Routes
 	// r.GET("/api/products", controllers.GetProducts)
@@ -17,7 +14,5 @@ func SetupRoutes(r *gin.Engine) {
 	r.PUT("/api/products/:id", controllers.UpdateProduct)
 	r.DELETE("/api/products/:id", controllers.DeleteProduct)
 	r.POST("/api/user/:id/profile-picture", controllers.UploadProfilePicture)
-	// Retailer Routes
-	r.GET("/api/retailers", controllers.GetRetailers)
 
 }
